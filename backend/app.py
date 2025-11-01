08565e43b85c082551663db329edbf34f134b7a2
William Tao <williambtao22@gmail.com>
2025-11-01 14:26:32 -0400

    Added remove blueprint to app

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from routes.upload import upload_bp
 from routes.rate import rate_bp
 from routes.download import download_bp
 from routes.retrieve import retrieve_bp
+from routes.remove import remove_bp
 
 REGISTRY_PATH = os.path.join(os.path.dirname(__file__), "registry.json")
 
@@ -20,6 +21,7 @@ app.register_blueprint(upload_bp)
 app.register_blueprint(rate_bp)
 app.register_blueprint(download_bp)
 app.register_blueprint(retrieve_bp)
+app.register_blueprint(remove_bp)
 
 
 @app.route('/')
